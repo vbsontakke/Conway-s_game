3b2999cbc650070a6ceb1f0609f6836d03787d4d
Vinayak Sontakke <vsontakk@in.ibm.com>
2019-11-16 11:15:01 +0530

    dsd~

---
+++ main_test.py
@@ -0,0 +1,10 @@
+import unittest2
+from "Conway-s_game.main" import * as m
+
+class TestMainMethods(unittest.TestCase):
+
+    def test_matrixExists(self):
+        self.assert()
+
+    def test_isMatrixValid(self):
+        res = 
